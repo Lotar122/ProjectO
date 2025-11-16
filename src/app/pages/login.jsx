68351b8c7e62e32a3b5f50fae0534bfc8c089a60
Lotar122 <dajlizaka9@gmail.com>
2025-11-16 18:04:14 +0100

    Once again...

--- src/app/pages/login.jsx
+++ src/app/pages/login.jsx
@@ -12,7 +12,7 @@ export default function Login({currentpage, setCurrentPage, isLoggedIn, setIsLog
 
 	useEffect(() => {
 		async function load() {
-		const res = await fetch("https://orto.lotar122.dev/kratos/public/self-service/login/api", {method: "GET", credentials: "include"}).then(r => r.json());
+		const res = await fetch("https://orto.lotar122.dev/kratos/public/self-service/login/browser", {method: "GET", credentials: "include"}).then(r => r.json());
 		setData(res);
 		}
 		load();
