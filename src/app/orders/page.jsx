b4543a241336aabf8273dfbadb60e0a42f81e06c
Lotar122 <dajlizaka9@gmail.com>
2025-11-17 21:14:29 +0100

    An attempt to make orders server protected by cookies

--- src/app/orders/page.jsx
+++ src/app/orders/page.jsx
@@ -1,3 +1,5 @@
+"use server";
+
 import React from "react";
 import { User, Package, Calendar, CheckCircle, Clock, XCircle, Eye, EyeOff, LogIn, LogOut, Plus, Search, Filter, ChevronDown, Home, FileText, UserCircle } from 'lucide-react';
 
