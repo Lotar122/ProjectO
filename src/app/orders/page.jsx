680bb27b8d265d18704a23fd4cc70787ea357000
Lotar122 <dajlizaka9@gmail.com>
2025-11-17 22:20:19 +0100

    An attempt to make orders server protected by cookies

--- src/app/orders/page.jsx
+++ src/app/orders/page.jsx
@@ -3,16 +3,15 @@
 // app/protected/page.tsx
 import { cookies } from 'next/headers';
 import axios from 'axios';
+import { redirect } from 'next/dist/server/api-utils';
 
-export default async function ProtectedPage({}) {
+export default async function ProtectedPage() {
   let cookieHeader = await cookies(); // get cookies from request
   cookieHeader = cookieHeader.get("ory_kratos_session");
 
   let loggedIn = false;
-  //try {
+  try {
     const kratosUrl = 'https://orto.lotar122.dev/kratos/public';
-
-    console.log(cookieHeader);
 
     const res = await axios.get(`${kratosUrl}/sessions/whoami`, {
       headers: {
@@ -20,18 +19,17 @@ export default async function ProtectedPage({}) {
       },
       withCredentials: false,
     });
-    console.log(res, cookieHeader);
 
     if (res.data && res.data.active) {
       loggedIn = true;
     }
-  //} catch (err) {
-    //loggedIn = false;
-  //}
+  } catch (err) {
+    loggedIn = false;
+  }
 
-  return (
-    <div>
-      {loggedIn ? <h1>Logged In</h1> : <h1>Not Logged In</h1>}
-    </div>
-  );
+  if(loggedIn)
+  {
+    return (<Orders ordersArray={ordersArray} ordersToBeDisplayed={ordersToBeDisplayed} />);
+  }
+  else redirect("/");
 }
