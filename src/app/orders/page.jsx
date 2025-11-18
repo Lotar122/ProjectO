4816dc88827b82d770017abe1ee1aba9a47e9dcc
Lotar122 <dajlizaka9@gmail.com>
2025-11-18 17:24:14 +0100

    Some changes.

--- src/app/orders/page.jsx
+++ src/app/orders/page.jsx
@@ -24,9 +24,11 @@ export default async function ProtectedPage() {
 
   let userAuthSession = await getUserAuthSession(cookieHeader);
 
+  console.log(userAuthSession);
+
   if(userAuthSession.loggedIn)
   {
     return (<Orders ordersArray={ordersArray} ordersToBeDisplayed={ordersToBeDisplayed} />);
   }
-  else redirect("/");
+  //else redirect("/");
 }
