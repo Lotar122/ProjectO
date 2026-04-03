c82a47395a56942df65e2fd3c8865782212f70ea
Lotar122 <dajlizaka9@gmail.com>
2026-04-03 18:19:07 +0200

    Changes

--- src/app/api/postOrder/route.js
+++ src/app/api/postOrder/route.js
@@ -2,7 +2,7 @@
 
 import { getUserAuthSession } from "@/app/server-functions/getUserAuthSession";
 import { createBucket } from "@/app/server-functions/MinIO/createBucket";
-import { createS3Client } from "@/app/server-functions/MinIO/getS3Client";
+import { createS3Client } from "@/app/server-functions/MinIO/createS3Client";
 import { cookies } from "next/headers";
 import postgres from "postgres";
 import { v7 as uuid7 } from "uuid";
