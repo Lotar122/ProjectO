2a22ea73d996a0fcc1f08c41b5d6a74191eee3dd
Lotar122 <dajlizaka9@gmail.com>
2026-04-03 12:54:17 +0200

    Changes

--- src/app/api/postOrder/route.js
+++ src/app/api/postOrder/route.js
@@ -38,7 +38,7 @@ export async function POST(req) {
       INSERT INTO orders (
         order_id, user_id, patient, details, status, progress, issue_date, due_date
       ) VALUES (
-        ${orderID}, ${userID}, ${patient}, ${details}, ${status}, ${progress}, ${issue_date}, ${due_date}
+        ${orderID}, ${userID}, ${patient}, ${details}, ${status}::order_status, ${progress}, ${issue_date}, ${due_date}
       )
     `;
 
