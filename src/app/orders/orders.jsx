772b26a0015b84a1a604685cd570894f5c14390d
Lotar122 <dajlizaka9@gmail.com>
2026-04-03 23:15:12 +0200

    Changes

--- src/app/orders/orders.jsx
+++ src/app/orders/orders.jsx
@@ -90,6 +90,7 @@ export default function Orders({ userName, userLastName }) {
 
 		nextOrders.forEach((val, index) => {
 			val.files.forEach((val, index) => {
+				console.log(val);
 				const response = axios.get(`/api/getFileName?file_id=${val.id}`,
 					{
 						withCredentials: true,
