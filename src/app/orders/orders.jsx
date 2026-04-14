128015f7a3beff7bc637730146c088a8ac2958de
Lotar122 <dajlizaka9@gmail.com>
2026-04-14 15:36:13 +0200

    Changes

--- src/app/orders/orders.jsx
+++ src/app/orders/orders.jsx
@@ -619,6 +619,17 @@ export default function Orders({ userName, userLastName }) {
 																			"No additional details yet."}
 																	</p>
 																</div>
+																<div>
+																	<button
+																		type="button"
+																		onClick={() => {
+																			
+																		}}
+																		className="inline-flex items-center gap-2 rounded-lg bg-white px-3 py-2 text-sm font-medium text-black transition-colors hover:bg-gray-200">
+																		<Download className="h-4 w-4" />
+																		Edit
+																	</button>
+																</div>
 															</div>
 
 															<div>
